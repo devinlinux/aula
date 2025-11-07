4b906ef8ce90227874f0699ea406cb3160eeada4
devinlinux <ssmith969898@gmail.com>
2025-11-07 18:58:34 -0500

    added id to user

--- backend/src/data/user.rs
+++ backend/src/data/user.rs
@@ -1,9 +1,10 @@
 use crate::data::Major;
 
 pub struct User {
+    id: u64,
     first_name: String,
     last_name: String,
-    //  PROFILE PICTURE
+    //  PROFILE PICTURE -> UUID + format auto convert?
     major: Major,
     graduation_year: i16,
 }
