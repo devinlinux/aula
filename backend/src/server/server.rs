c81a368c4c26a0ec23aa86f11729f9ca6f0f4c33
devinlinux <ssmith969898@gmail.com>
2025-11-10 16:22:23 -0500

    Created basic web server

---
+++ backend/src/server/server.rs
@@ -0,0 +1,32 @@
+use std::net::TcpListener;
+
+use actix_web::dev::Server;
+use actix_web::{ web, App, HttpServer };
+
+const MAX_SIZE: usize = 4096;
+
+pub struct WebServer;
+
+pub enum ServerMode {
+    Genesis,
+    Recovery,
+}
+
+impl WebServer {
+    pub fn run(listener: TcpListener, mode: ServerMode) -> std::io::Result<Server> {
+        let server = HttpServer::new(move || {
+            App::new()
+                .app_data(web::JsonConfig::default().limit(MAX_SIZE))
+                .wrap(
+                    actix_cors::Cors::default()
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header()
+                    .max_age(3600)
+                )
+        })
+        .listen(listener)?.run();
+
+        Ok(server)
+    }
+}
