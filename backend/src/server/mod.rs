c223d4e368da6dd350b954e572fa0d6ef3cd7eb3
devinlinux <ssmith969898@gmail.com>
2025-11-10 18:07:26 -0500

    Added tests for health check route

--- backend/src/server/mod.rs
+++ backend/src/server/mod.rs
@@ -4,3 +4,15 @@ mod server;
 
 pub use server::WebServer;
 pub use server::ServerMode;
+
+pub(super) const TEST_PORT: u16 = 8081;
+
+#[cfg(test)]
+pub(super) fn test_spawn() {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8081")
+        .expect("Failed to bind to port 8081, may be in use");
+
+    let server = WebServer::run(listener, ServerMode::Genesis).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+}
--- backend/src/server/routes/health_check.rs
+++ backend/src/server/routes/health_check.rs
@@ -4,3 +4,21 @@ use actix_web::{ get, HttpResponse, Responder };
 pub async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
+
+#[cfg(test)]
+mod tests {
+    #[actix_web::test]
+    async fn test_health_check() {
+        crate::server::test_spawn();
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(&format!("127.0.0.1:{}/health_check", crate::server::TEST_PORT))
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert!(response.status().is_success());
+        assert_eq!(Some(0), response.content_length());
+    }
+}
