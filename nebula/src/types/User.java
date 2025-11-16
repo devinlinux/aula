bb7d79fdc7f893f9fc39c7d9930dc80d94e5a78e
devinlinux <ssmith969898@gmail.com>
2025-11-16 16:55:41 -0500

    Implemented compareTo for user

--- nebula/src/types/User.java
+++ nebula/src/types/User.java
@@ -1,4 +1,8 @@
 package types;
 
-public record User(long id, String email, String firstName, String lastName, int graduationYear) {
+public record User(Long id, String email, String firstName, String lastName, int graduationYear) implements Comparable<User> {
+    @Override
+    public int compareTo(User other) {
+        return this.id.compareTo(other.id);
+    }
 }
