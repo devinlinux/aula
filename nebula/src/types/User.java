693296abffaae6666ede9e5f2a375d26b84b8319
devinlinux <ssmith969898@gmail.com>
2025-11-16 16:58:51 -0500

    Converted user to class

--- nebula/src/types/User.java
+++ nebula/src/types/User.java
@@ -1,6 +1,24 @@
 package types;
 
-public record User(Long id, String email, String firstName, String lastName, int graduationYear) implements Comparable<User> {
+public class User implements Comparable<User> {
+    private final Long id;
+    private final String email;
+    private final String firstName;
+    private final String lastName;
+    private final int graduationYear;
+
+    public User(long id, String email, String firstName, String lastName, int graduationYear) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.graduationYear = graduationYear;
+    }
+
+    public long id() {
+        return this.id;
+    }
+
     @Override
     public int compareTo(User other) {
         return this.id.compareTo(other.id);
