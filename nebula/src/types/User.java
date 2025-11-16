45a250a89e7b64c611e8d2a101b238878ec3b239
devinlinux <ssmith969898@gmail.com>
2025-11-16 17:08:06 -0500

    Added groups to user

--- nebula/src/types/User.java
+++ nebula/src/types/User.java
@@ -1,11 +1,14 @@
 package types;
 
+import java.util.ArrayList;
+
 public class User implements Identifiable, Comparable<User> {
     private final Long id;
     private final String email;
     private final String firstName;
     private final String lastName;
     private final int graduationYear;
+    private final ArrayList<Group> groups;
 
     public User(long id, String email, String firstName, String lastName, int graduationYear) {
         this.id = id;
@@ -13,6 +16,11 @@ public class User implements Identifiable, Comparable<User> {
         this.firstName = firstName;
         this.lastName = lastName;
         this.graduationYear = graduationYear;
+        this.groups = new ArrayList<>();
+    }
+
+    public void addGroup(Group group) {
+        this.groups.add(group);
     }
 
     public long id() {
