24c6d85b631484f03da3c2556a879d8755e9a8d2
devinlinux <ssmith969898@gmail.com>
2025-11-17 21:45:32 -0500

    Created new java project

---
+++ coalesence/src/main/java/com/michaelb/coalesence/CoalesenceApplication.java
@@ -0,0 +1,13 @@
+package com.michaelb.coalesence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoalesenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoalesenceApplication.class, args);
+	}
+
+}
---
+++ coalesence/src/test/java/com/michaelb/coalesence/CoalesenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelb.coalesence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoalesenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
