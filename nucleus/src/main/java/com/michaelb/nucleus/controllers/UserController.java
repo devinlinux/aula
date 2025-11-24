40cd73f3cf3f6d6b9b07039b679c16f765713051
devinlinux <ssmith969898@gmail.com>
2025-11-24 16:18:53 -0500

    Added getters and setters for User model

--- nucleus/src/main/java/com/michaelb/nucleus/controllers/UserController.java
+++ nucleus/src/main/java/com/michaelb/nucleus/controllers/UserController.java
@@ -1,4 +1,26 @@
 package com.michaelb.nucleus.controllers;
 
+// imports
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.michaelb.nucleus.models.User;
+import com.michaelb.nucleus.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService service;
+    private final ConcurrentHashMap<String, Boolean> activeSessions;
+
+    public UserController(UserService service) {
+        this.service = service;
+        this.activeSessions = new ConcurrentHashMap<>();
+    }
 }
--- nucleus/src/main/java/com/michaelb/nucleus/models/User.java
+++ nucleus/src/main/java/com/michaelb/nucleus/models/User.java
@@ -23,7 +23,67 @@ public class User {
     private Integer graduationYear;
     private String profilePicture;
 
-    public void profilePicture(String profilePicture) {
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getHashedPassword() {
+        return this.hashedPassword;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getMajor() {
+        return this.major;
+    }
+
+    public void setGraduationYear(Integer graduationYear) {
+        this.graduationYear = graduationYear;
+    }
+
+    public Integer getGraduationYear() {
+        return this.graduationYear;
+    }
+
+    public void setProfilePicture(String profilePicture) {
         this.profilePicture = profilePicture;
+    }
+
+    public String getProfilePicture() {
+        return this.profilePicture;
     }
 }
