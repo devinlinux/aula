4f32e2f65dc25bceea4f1a6b41873f3a88a137bb
devinlinux <ssmith969898@gmail.com>
2025-11-28 21:06:07 -0500

    Created container component for groups

---
+++ frontend/src/components/ui/group-item.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import {
+    Box,
+    Image,
+    LinkBox,
+    LinkOverlay,
+    Text,
+} from "@chakra-ui/react"
+import NextLink from "next/link"
+import { Global } from "@emotion/react"
+
+const GroupItem = ({ id, name, associatedClass, times, bannerImage }) => {
+    <Box w="100%" textAlign="center">
+        <LinkBox
+            as={NextLink}
+            href={`/groups/${id}`}
+            scroll={false}
+            cursor="pointer"
+        >
+            <Image
+                src={bannerImage}
+                alt={name}
+                className="group-item-banner-image"
+                placeholder="blur"
+                loading="lazy"
+            />
+            <LinkOverlay as="div" href={`/groups/${id}`}>
+                <Text mt={2} fontSize={20}>
+                    {name}
+                </Text>
+            </LinkOverlay>
+        </LinkBox>
+    </Box>
+}
+
+const GroupItemStyle = () => {
+    <Global
+        style={`
+            .group-item-banner-image {
+                border-radius: 12px;
+            }
+        `}
+    />
+}
+
+export default GroupItem
