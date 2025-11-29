c3f58b526592fdfb648019a85c6ddd4fff1111f9
devinlinux <ssmith969898@gmail.com>
2025-11-29 00:49:35 -0500

    Fixed error with MeetingTime

--- nucleus/src/main/java/com/michaelb/nucleus/dto/CreateGroupDTO.java
+++ nucleus/src/main/java/com/michaelb/nucleus/dto/CreateGroupDTO.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 
 import com.michaelb.nucleus.models.Group;
 
-public record CreateGroupDTO(String name, String associatedClass, ArrayList<MeetingTime> times, String creator) {
+public record CreateGroupDTO(String name, String associatedClass, ArrayList<String> times, String creator) {
     public Group intoGroup() {
         return new Group(
                 name,
                 associatedClass,
                 times,
-                new ArrayList<String>(),
+                new ArrayList<>(),
                 creator,
                 null
         );
