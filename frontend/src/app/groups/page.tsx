ab8fcca547d7a91c4f1b2fec1c018a8783b877fd
devinlinux <ssmith969898@gmail.com>
2025-11-28 20:57:21 -0500

    Created groups page

---
+++ frontend/src/app/groups/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {
+    Container,
+    Box,
+} from "@chakra-ui/react"
+
+const Groups = () => {
+    return (
+        <Container maxW="container.lg">
+        </Container>
+    )
+}
+
+export default Groups
